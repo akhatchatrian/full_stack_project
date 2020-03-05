8920e81e890f1be90cee2dc655fb4ad4c6458103
Arman Khatchatrian <armank0089@gmail.com>
2020-03-04 17:46:55 -0800

    herokufix

--- db/migrate/20200303175512_change_style_profile_values.rb
+++
@@ -1,6 +0,0 @@
-class ChangeStyleProfileValues < ActiveRecord::Migration[5.2]
-  def change
-    change_column :style_profile_values, :user_id, :integer, :null=>true
-    change_column :style_profile_values, :value, :string, :null=>true
-  end
-end
