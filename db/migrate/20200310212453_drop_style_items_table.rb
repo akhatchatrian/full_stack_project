e7281d0cbb9120cd7062156d8aaa9b8a2de9dae7
Arman Khatchatrian <armank0089@gmail.com>
2020-03-13 09:05:42 -0700

    heroku errors again

--- db/migrate/20200310212453_drop_style_items_table.rb
+++
@@ -1,5 +0,0 @@
-class DropStyleItemsTable < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :style_profile_items
-  end
-end
--- db/migrate/20200310212615_drop_style_values_table.rb
+++
@@ -1,5 +0,0 @@
-class DropStyleValuesTable < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :style_profile_values
-  end
-end
